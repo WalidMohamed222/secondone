c766492ad39f21cb34c9f98ff3d70a81965739fd
WalidMohamed222 <158574139+WalidMohamed222@users.noreply.github.com>
2024-02-07 03:33:07 +0200

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:secondone/views/screens/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +12,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       title: 'Creativa App',
       theme: ThemeData(
